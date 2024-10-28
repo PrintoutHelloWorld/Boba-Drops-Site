f11240211035e280f385758907b47d2bc912e199
PrintoutHelloWorld <ansun@bbns.org>
2024-10-28 14:37:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, world!");
+function dog() {
+  const text = document.getElementById("dog");
+  text.innerHTML += '<img classname = "dog" style = "height:400px" src = "https://cdn.glitch.global/e2317e81-509a-4c13-8c21-c843a5c7ab84/IMG_0983.png?v=1729534801398" /> '
+}  
